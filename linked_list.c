179737fdc5bec5ea78b953e14e157063042c0ce8
Shehan Samarasena <57752134+ShehanCodes@users.noreply.github.com>
2023-08-07 10:08:47 +0200

    Add files via upload

---
+++ linked_list.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <limits.h>
+
+typedef struct node{
+    int data;
+    struct node *next;
+}
+node;
+
+
+int main(void)
+{
+    node *list = NULL;
+
+    while (true)
+    {
+        int x;
+        printf("Number: \n");
+        scanf("%d",&x);
+        if (x == INT_MAX)
+        {
+            printf("\n");
+            break;
+        }
+
+        // Allocate a new node
+        node *newNode = malloc(sizeof(node));
+        if (newNode == NULL)
+        {
+            return 1;
+        }
+        newNode->data = x;
+        newNode->next = NULL;
+
+        newNode->next = list;
+        list = newNode;
+        // Add new node to head of linked list
+
+    }
+
+    // Print all nodes
+    for (node *trav = list; trav != NULL; trav = trav->next)
+    {
+        printf("%i\n", trav->data);
+    }
+    // Free all nodes
+    node *trav = list;
+    while (trav != NULL)
+    {
+        node *tmp = trav;
+        trav = trav->next;
+        free(tmp);
+    }
+
+}
